99974ecb02cf3524ecced41441be922516f9ac93
takahawk <takahawkkun@gmail.com>
2016-03-09 23:34:50 +0200

    Storks added

--- core/src/com/bitbucket/iddqdteam/smartassstork/game/PlayerActor.java
+++ core/src/com/bitbucket/iddqdteam/smartassstork/game/PlayerActor.java
@@ -1,9 +1,11 @@
 package com.bitbucket.iddqdteam.smartassstork.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.bitbucket.iddqdteam.smartassstork.screens.GameScreen;
@@ -12,23 +14,54 @@ import com.bitbucket.iddqdteam.smartassstork.screens.GameScreen;
  * Created by takahawk on 08.03.16.
  */
 public class PlayerActor extends Actor {
+    private static final float MOVE_DELAY_TIME = 0.5f;
     private static final float MOVE_FORCE = 4f;
-    private static final float JUMP_FORCE = 1f;
-    Sprite _sprite;
+    private static final float JUMP_FORCE = 2f;
+    TextureRegion texture;
+    Animation moveAnimation;
     Body _body;
+    private boolean rightOrientation = true;
     private boolean onTheGround = false;
+    private float lastMove = 0;
+    private float elapsedTime = 0;
 
     public PlayerActor(final TextureRegion texture, final Body body) {
-        _sprite = new Sprite(texture);
+        this.texture = texture;
         _body = body;
     }
+
+    public void setMoveAnimation(Animation moveAnimation) {
+        this.moveAnimation = moveAnimation;
+    }
+
+
     @Override
     public void draw(Batch batch, float parentAlpha) {
+        TextureRegion frame;
+
+        if ((elapsedTime - lastMove > MOVE_DELAY_TIME) || !onTheGround)
+            frame = texture;
+        else {
+            frame = moveAnimation.getKeyFrame(elapsedTime, true);
+        }
+        if (!rightOrientation) {
+            frame.flip(true, false);
+        }
         batch.draw(
-                _sprite.getTexture(),
-                _body.getPosition().x * GameScreen.PIXELS_TO_METERS - _sprite.getWidth() / 2,
-                _body.getPosition().y * GameScreen.PIXELS_TO_METERS - _sprite.getHeight() / 2
+                frame,
+                _body.getPosition().x * GameScreen.PIXELS_TO_METERS - texture.getRegionWidth() / 2,
+                _body.getPosition().y * GameScreen.PIXELS_TO_METERS - texture.getRegionHeight() / 2
         );
+
+        if (!rightOrientation) {
+            frame.flip(true, false);
+        }
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+        elapsedTime += delta;
     }
 
     @Override
@@ -42,10 +75,14 @@ public class PlayerActor extends Actor {
     }
 
     public void moveRight() {
+        rightOrientation = true;
+        lastMove = elapsedTime;
         _body.applyForceToCenter(MOVE_FORCE, 0, true);
     }
 
     public void moveLeft() {
+        rightOrientation = false;
+        lastMove = elapsedTime;
         _body.applyForceToCenter(-MOVE_FORCE, 0, true);
     }
 
--- core/src/com/bitbucket/iddqdteam/smartassstork/screens/GameScreen.java
+++ core/src/com/bitbucket/iddqdteam/smartassstork/screens/GameScreen.java
@@ -6,13 +6,12 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.objects.PolygonMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Matrix4;
@@ -20,11 +19,14 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.bitbucket.iddqdteam.smartassstork.game.PlayerActor;
 import com.bitbucket.iddqdteam.smartassstork.game.PlayerData;
+import com.bitbucket.iddqdteam.smartassstork.game.Stork;
+import com.bitbucket.iddqdteam.smartassstork.util.ResourceManager;
 import com.bitbucket.iddqdteam.smartassstork.util.TiledMapParser;
 
 import java.util.Arrays;
@@ -35,21 +37,51 @@ import java.util.Iterator;
  */
 public class GameScreen implements Screen {
     public static final float PIXELS_TO_METERS = 100;
-    private static final float PIXEL_PER_TILES = 64;
     private static final float GRAVITY = -5f;
+    private ResourceManager resourceManager;
     private OrthographicCamera camera = initCamera();
+    private OrthographicCamera camera2 = new OrthographicCamera();
     private Viewport port = new FillViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), camera);
     private World world = new World(new Vector2(0, GRAVITY), true);
     private TiledMapRenderer mapRenderer;
     private TiledMapParser mapParser = new TiledMapParser();
-    private Texture heroTexture = new Texture("sprite.png");
+    private Texture heroTexture;
+    private Texture moonTexture;
+    private Texture storkTexture;
+    private Animation heroMoveAnimation;
     private PlayerActor playerActor;
     private Stage stage;
+    private int mapWidth, mapHeight;
+    private int tileWidth, tileHeight;
+    private SpriteBatch batch;
+
+    private boolean gameIsOver = false;
 
     private Box2DDebugRenderer debugRenderer = new Box2DDebugRenderer();
 
+    public void initResources() {
+        heroTexture = resourceManager.get("hero1.png", Texture.class);
+        heroMoveAnimation = new Animation(
+                0.1f,
+                new Array<TextureRegion>(new TextureRegion[] {
+                        new TextureRegion(resourceManager.get("hero1.png", Texture.class)),
+                        new TextureRegion(resourceManager.get("hero2.1.png", Texture.class)),
+                        new TextureRegion(resourceManager.get("hero2.png", Texture.class))
+                })
+                );
+        moonTexture = resourceManager.get("sky_tileset1.png", Texture.class);
+        storkTexture = resourceManager.get("stork-flying.png", Texture.class);
+    }
 
-    public GameScreen(PlayerData playerData, TiledMap map) {
+    public GameScreen(PlayerData playerData, TiledMap map, ResourceManager resourceManager) {
+        this.resourceManager = resourceManager;
+        this.batch = new SpriteBatch();
+        TiledMap foregroundMap = new TmxMapLoader().load("light.tmx");
+        initResources();
+        tileWidth = (Integer) map.getProperties().get("tilewidth");
+        tileHeight = (Integer) map.getProperties().get("tileheight");
+        mapWidth = (Integer) map.getProperties().get("width") * tileWidth;
+        mapHeight = (Integer) map.getProperties().get("height") * tileHeight;
         mapRenderer = new OrthogonalTiledMapRenderer(map);
         stage = new Stage(port);
         BodyDef bodyDef = new BodyDef();
@@ -67,28 +99,32 @@ public class GameScreen implements Screen {
         fixture.density = 0.0f;
         fixture.restitution = 0f;
         playerBody.createFixture(fixture);
+        playerBody.setUserData("hero");
         shape.dispose();
         playerActor = new PlayerActor(new TextureRegion(heroTexture), playerBody);
+        playerActor.setMoveAnimation(heroMoveAnimation);
         stage.addActor(playerActor);
         world.setContactListener(new ContactListener() {
+            private boolean with(Contact contact, String that) {
+                return  that.equals(contact.getFixtureA().getBody().getUserData().toString()) ||
+                        that.equals(contact.getFixtureB().getBody().getUserData().toString());
+            }
+
             @Override
             public void beginContact(Contact contact) {
-                if (
-                        playerBody.getFixtureList().contains(contact.getFixtureA(), true) ||
-                        playerBody.getFixtureList().contains(contact.getFixtureB(), true)
-                        ) {
-                    if (MathUtils.isEqual(MathUtils.floor(contact.getWorldManifold().getNormal().y), -1))
-                        playerActor.setOnTheGround(true);
-                }
-
+                if (with(contact, "hero"))
+                    if (with(contact, "ground")) {
+                        if (MathUtils.isEqual(MathUtils.floor(contact.getWorldManifold().getNormal().y), -1))
+                            playerActor.setOnTheGround(true);
+                    }
+                    else if (with(contact, "stork")) {
+                        gameIsOver = true;
+                    }
             }
 
             @Override
             public void endContact(Contact contact) {
-                if (
-                        playerBody.getFixtureList().contains(contact.getFixtureA(), true) ||
-                                playerBody.getFixtureList().contains(contact.getFixtureB(), true)
-                        )
+                if (with(contact, "ground") && with(contact, "hero"))
                     playerActor.setOnTheGround(false);
             }
 
@@ -103,6 +139,7 @@ public class GameScreen implements Screen {
             }
         });
         addGround(map);
+        addStorks(map);
     }
 
     public void addGround(TiledMap map) {
@@ -114,10 +151,39 @@ public class GameScreen implements Screen {
                 bd.type = BodyDef.BodyType.StaticBody;
                 Body body = world.createBody(bd);
                 body.createFixture(shape, 1);
+                body.setUserData("ground");
                 shape.dispose();
             }
 
 
+        }
+    }
+
+    public void addStorks(TiledMap map) {
+        for (MapObject obj : map.getLayers().get("Enemies").getObjects()) {
+            BodyDef bodyDef = new BodyDef();
+            bodyDef.type = BodyDef.BodyType.DynamicBody;
+            bodyDef.gravityScale = 0;
+            bodyDef.position.set(
+                    (Float) obj.getProperties().get("x") / PIXELS_TO_METERS,
+                    (Float) obj.getProperties().get("y") / PIXELS_TO_METERS
+            );
+            final Body storkBody = world.createBody(bodyDef);
+
+            PolygonShape shape = new PolygonShape();
+            shape.setAsBox(storkTexture.getWidth() / 2 / PIXELS_TO_METERS, storkTexture.getHeight() / 2 / PIXELS_TO_METERS);
+            FixtureDef fixture = new FixtureDef();
+            fixture.shape = shape;
+            fixture.density = 0.0f;
+            fixture.restitution = 0f;
+            storkBody.createFixture(fixture);
+            shape.dispose();
+            storkBody.setUserData("stork");
+            stage.addActor(new Stork(
+                    new TextureRegion(storkTexture),
+                    storkBody,
+                    (Integer.parseInt((String) obj.getProperties().get("routeDistance"))) * tileWidth / PIXELS_TO_METERS
+            ));
         }
     }
 
@@ -135,13 +201,27 @@ public class GameScreen implements Screen {
     public void handleInput() {
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
             playerActor.moveRight();
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+        } else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
             playerActor.moveLeft();
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+        } else if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
             playerActor.jump();
+        } else {
         }
+    }
+
+    public void centerCamera() {
+        camera.position.set(playerActor.getX(), playerActor.getY(), 0);
+        camera.update();
+        camera.position.x = MathUtils.clamp(
+                camera.position.x,
+                camera.viewportWidth / 2,
+                mapWidth - camera.viewportWidth / 2
+                );
+        camera.position.y = MathUtils.clamp(
+                camera.position.y,
+                camera.viewportHeight / 2,
+                mapHeight - camera.viewportHeight / 2
+        );
     }
 
     @Override
@@ -151,16 +231,20 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        handleInput();
-        world.step(delta, 6, 2);
+        if (!gameIsOver) {
+            handleInput();
+            world.step(delta, 6, 2);
+        }
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        camera.position.set(playerActor.getX(), playerActor.getY(), 0);
-        camera.update();
+
+
         mapRenderer.setView(camera);
         mapRenderer.render();
+        centerCamera();
         stage.act(delta);
         stage.draw();
+
         Matrix4 debugMatrix = camera.combined.cpy().scale(PIXELS_TO_METERS, PIXELS_TO_METERS, 0);
         debugRenderer.render(world, debugMatrix);
     }
