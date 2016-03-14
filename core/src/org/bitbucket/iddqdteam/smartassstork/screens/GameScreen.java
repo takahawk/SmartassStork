8f06a10452df3ef1b2190938aa6dc96c50d1ca30
takahawk <takahawkkun@gmail.com>
2016-03-15 01:04:23 +0200

    buglist updated

--- core/src/org/bitbucket/iddqdteam/smartassstork/screens/GameScreen.java
+++ core/src/org/bitbucket/iddqdteam/smartassstork/screens/GameScreen.java
@@ -533,7 +533,7 @@ public class GameScreen
             update(delta);
         }
         handleInput();
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         mapStage.act(delta);
